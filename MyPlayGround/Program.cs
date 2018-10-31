78961f6537757239c9faeeef5e23b1c87b468298
marcel m grilo <marcelgrilo86@gmail.com>
2018-10-30 21:27:45 -0300

    .

---
+++ MyPlayGround/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyPlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
